e1e2f8b34b6de4c7c107503dafe82b85603af099
falk <fhartmann@gmx.de>
2022-09-14 18:14:47 +0200

    Extending test.

--- src/test/java/org/jugsaxony/treed/MyStrategyTest.java
+++ src/test/java/org/jugsaxony/treed/MyStrategyTest.java
@@ -1,5 +1,6 @@
 package org.jugsaxony.treed;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -12,8 +13,27 @@ public class MyStrategyTest {
   @Test
   public void testAuthorEmail() {
     assertNotNull(myStrategy.getAuthorEmail()); 
+    assertNotEquals("", myStrategy.getAuthorEmail().trim()); 
   }
-  
+
+  @Test
+  public void testAuthorName() {
+    assertNotNull(myStrategy.getAuthorName());
+    assertNotEquals("", myStrategy.getAuthorName().trim()); 
+  }
+
+  @Test
+  public void testDescription() {
+    assertNotNull(myStrategy.getDescription());
+    assertNotEquals("", myStrategy.getDescription().trim()); 
+  }
+
+  @Test
+  public void testStrategyName() {
+    assertNotNull(myStrategy.getStrategyName());
+    assertNotEquals("", myStrategy.getStrategyName().trim()); 
+  }
+
   @Test
   public void testReadyToSubmit() {
     assertTrue(myStrategy.readyToSubmit());
