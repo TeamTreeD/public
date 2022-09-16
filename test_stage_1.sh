e8f8c1d97c07cf4dcea2329b4f59339f6bec0bf0
falk <fhartmann@gmx.de>
2022-09-16 16:19:40 +0200

    New trial.

--- test_stage_1.sh
+++ test_stage_1.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 report_file="target/surefire-reports/TEST-org.jugsaxony.treed.MyStrategyTest.xml"
 error_file="error.txt"
-boot_file="boot.txt"
 
-mvn spring-boot:stop & > ${boot_file} 2&>1
+mvn spring-boot:stop & > ${error_file} 2&>1
 
 
-mvn -q -Dtest=org.jugsaxony.treed.MyStrategyTest clean surefire-report:report > ${error_file} 2>&1
+mvn -q -Dtest=org.jugsaxony.treed.MyStrategyTest clean surefire-report:report >> ${error_file} 2>&1
 if [ -e ${report_file} ]
 then
     cat ${report_file}
@@ -15,4 +14,4 @@ else
 fi
 
 # Run spring boot in background, exposing at 8037
-nohup mvn spring-boot:start & >> ${boot_file} 2&>1
+nohup mvn spring-boot:start
